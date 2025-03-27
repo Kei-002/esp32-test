98d53a59040e59024218a9fccf5c647938189677
Khen Christian Vargas <khenchristian.vargas@gmail.com>
2025-03-27 22:24:31 +0800

    working webapp

--- main/main.ino
+++ main/main.ino
@@ -1,172 +1,224 @@
-#include <DHT.h>
-#include <ESP32Servo.h>
-// for wireless capabilities
 #include <WiFi.h>
-#include <ESPAsyncWebServer.h>
-#include <SPIFFS.h>
-// Define the pins
-// MAKE SURE TO CHANGE THIS PINS AS THEY ARE ALL THE SAME
-#define MOISTURE_SENSOR_PIN  34  // Use any ADC pin of ESP32
-#define PUMP_RELAY_PIN 23  // Pin connected to the relay
+#include <Firebase_ESP_Client.h>
+#include <DHT.h>
+#include <ArduinoJson.h>
+#include <ESP32Servo.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
 
-#define LDR_PIN 34  // ADC1 pin for LDR
-#define GROWLIGHT_RELAY_PIN 23 // Relay control pin
+// WiFi credentials
+const char* ssid = "YourWiFiSSID";
+const char* password = "YourWiFiPassword";
 
-// place a pin here
-#define DHT11_PIN = _
-#define SERVO_PIN = _
-#define FAN_PIN = _
+// Firebase credentials
+#define API_KEY "YOUR-FIREBASE-API-KEY"
+#define DATABASE_URL "YOUR-FIREBASE-DATABASE-URL"
 
-// Define what pins the module uses
-DHT dht11(DHT11_PIN, DHT11);
-Servo servoMotor;
+// Pin definitions
+#define DHT_PIN 4
+#define DHT_TYPE DHT22
+#define SOIL_MOISTURE_PIN 34
+#define LDR_PIN 35
+#define PUMP_PIN 16
+#define GROW_LIGHTS_PIN 17
+#define FAN_PIN 18
+#define SERVO_PIN 19
 
-#define DARK_VALUE 4000  // Adjust based on testing (dark environment)
-#define BRIGHT_VALUE 500  // Adjust based on testing (bright environment)
+// Initialize components
+DHT dht(DHT_PIN, DHT_TYPE);
+Servo windowServo;
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
-// WIFI Setup
-// Replace with your network credentials
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
-
-AsyncWebServer server(80);
-
-
-// Variables for readings
-float moisturePercent = 0.0;
-int lightPercent = 0;
-float dht11SensorValue = 0;
-
-void setupWifi() {
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting...");
-  }
-  Serial.println(WiFi.localIP());
-
-  // Start SPIFFS
-  if (!SPIFFS.begin()) {
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
-
-  // Serve static files
-  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
-
-  // Sensor data endpoint
-  server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
-    String json = "{\"temp\": " + String(dht11SensorValue) + 
-                  ", \"moisture\": " + String(moisturePercent) + 
-                  ", \"light\": " + String(lightPercent) + "}";
-    request->send(200, "application/json", json);
-  });
-
-  server.begin();
-}
-
-void controlPumpByMoisture(float moisturePercent) {
-    Serial.print("Moisture Level: ");
-    Serial.print(moisturePercent);
-    Serial.println("%");
-
-    if (moisturePercent < 30) {
-        digitalWrite(PUMP_RELAY_PIN, LOW); // Turn pump on
-        Serial.println("Pump ON");
-    } else if (moisturePercent >= 50) {
-        digitalWrite(PUMP_RELAY_PIN, HIGH); // Turn pump off
-        Serial.println("Pump OFF");
+    Serial.begin(115200);
+    
+    // Initialize SPIFFS
+    if(!SPIFFS.begin(true)) {
+        Serial.println("SPIFFS Mount Failed");
+        return;
     }
-}
 
-// Manage the ldr and growlights here
-void controlGrowlightsWithLDR(int lightPercent) {
-    Serial.print("Light Intensity: ");
-    Serial.print(lightPercent);
-    Serial.println("%");
-
-    // Control relay based on light intensity
-    if (lightPercent < 50) {  // Example threshold
-        digitalWrite(GROWLIGHT_RELAY_PIN, LOW); // Turn on relay in darkness
-    } else {
-        digitalWrite(GROWLIGHT_RELAY_PIN, HIGH);  // Turn off relay in brightness
-    }
-}
-
-// Manage fan/servo and dht11 here
-void controlFanServoByTemperature(float temp) {
-    // Freely change the values that best fits your needs ;)
-    // hot hot hot means turn on fan and window(servo)
-    if (temp >= 30) {
-      // Open window by setting servo angle to 180deg
-      
-      servoMotor.write(160);
-      delay(20);
-
-      // Turn on fan here
-      digitalWrite(FAN_PIN, HIGH); 
-    }
-    else {
-      // Close window by setting servo to 0deg
-      servoMotor.write(0);
-      delay(20);
-
-      // turn off fan here
-      digitalWrite(FAN_PIN, LOW);
-    }
-}
-
-void setup() {
-    Serial.begin(9600); // Start serial communication
-    pinMode(PUMP_RELAY_PIN, OUTPUT);
-    digitalWrite(PUMP_RELAY_PIN, LOW); // Ensure pump is off initially
-
-    pinMode(GROWLIGHT_RELAY_PIN, OUTPUT);
-    digitalWrite(GROWLIGHT_RELAY_PIN, LOW); // Ensure relay is off initially
-
-    dht11.begin(); // initialize the DHT11 sensor
-    servoMotor.attach(SERVO_PIN);  // initialize the servo  
-
+    // Initialize pins
+    pinMode(PUMP_PIN, OUTPUT);
+    pinMode(GROW_LIGHTS_PIN, OUTPUT);
     pinMode(FAN_PIN, OUTPUT);
-    digitalWrite(FAN_PIN, LOW);  // Fan off initially
+    
+    // Initialize servo
+    windowServo.attach(SERVO_PIN);
+    windowServo.write(0); // Close window by default
+    
+    // Connect to WiFi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+    Serial.println("IP address: " + WiFi.localIP().toString());
 
-    setupWifi();
+    // Initialize DHT sensor
+    dht.begin();
+
+    // Route for root directory and static files
+    server.on("/", HTTP_GET, handleRoot);
+    server.on("/bootstrap.min.css", HTTP_GET, []() {
+        File file = SPIFFS.open("/bootstrap.min.css", "r");
+        server.streamFile(file, "text/css");
+        file.close();
+    });
+    server.on("/styles.css", HTTP_GET, []() {
+        File file = SPIFFS.open("/styles.css", "r");
+        server.streamFile(file, "text/css");
+        file.close();
+    });
+    server.on("/jquery-3.7.1.min.js", HTTP_GET, []() {
+        File file = SPIFFS.open("/jquery-3.7.1.min.js", "r");
+        server.streamFile(file, "application/javascript");
+        file.close();
+    });
+    server.on("/bootstrap.min.js", HTTP_GET, []() {
+        File file = SPIFFS.open("/bootstrap.min.js", "r");
+        server.streamFile(file, "application/javascript");
+        file.close();
+    });
+    server.on("/script.js", HTTP_GET, []() {
+        File file = SPIFFS.open("/script.js", "r");
+        server.streamFile(file, "application/javascript");
+        file.close();
+    });
+
+    // Routes for sensor data and control
+    server.on("/data", HTTP_GET, handleData);
+    server.on("/window", HTTP_POST, handleWindow);
+    server.on("/fan", HTTP_POST, handleFan);
+    server.on("/wateringlog", HTTP_GET, handleWateringLog);
+
+    server.begin();
 }
 
 void loop() {
-    int moistureSensorValue = analogRead(MOISTURE_SENSOR_PIN); // Read the sensor value
-    moisturePercent = map(moistureSensorValue, 4095, 1500, 0, 100); // Convert to percentage (adjust based on calibration)
-    int ldrValue = analogRead(LDR_PIN); // Read LDR value
-    lightPercent = map(ldrValue, DARK_VALUE, BRIGHT_VALUE, 0, 100);
-    dht11SensorValue = dht11.readTemperature();
-
-    // Ensure values stay within 0-100%
-    moisturePercent = constrain(moisturePercent, 0, 100);
-    lightPercent = constrain(lightPercent, 0, 100);
-
-    // Control the pump based on moisture levels
-    controlPumpByMoisture(moisturePercent);
-    
-    // Control the growlights based on the light outside
-    controlGrowlightsWithLDR(lightPercent);
-
-    // Control fan based on temperature
-    controlFanServoByTemperature(dht11SensorValue);
-
-    // Wait a second before reading again
-    delay(1000); 
+    server.handleClient();
+    checkAndControlDevices();
+    delay(100);
 }
 
-// Manage the Pump and Moisture Sensor here
+void checkAndControlDevices() {
+    float moisture = getMoisture();
+    float temperature = dht.readTemperature();
+    
+    // Automatic window control based on temperature
+    if (temperature > 30) {
+        windowServo.write(90); // Open window
+    } else if (temperature < 25) {
+        windowServo.write(0); // Close window
+    }
+    
+    // Fan control based on temperature
+    if (temperature > 28) {
+        digitalWrite(FAN_PIN, HIGH);
+    } else {
+        digitalWrite(FAN_PIN, LOW);
+    }
+    
+    // Water pump control and logging
+    if (moisture < 30) { // If soil is too dry
+        digitalWrite(PUMP_PIN, HIGH);
+        // Log watering event
+        if (millis() - lastWateringTime > 300000) { // 5 minutes between logs
+            logWatering(moisture);
+            lastWateringTime = millis();
+        }
+    } else {
+        digitalWrite(PUMP_PIN, LOW);
+    }
+}
 
+void logWatering(float moisture) {
+    time_t now;
+    time(&now);
+    wateringLogs[currentLogIndex].timestamp = now;
+    wateringLogs[currentLogIndex].moisture = moisture;
+    
+    currentLogIndex = (currentLogIndex + 1) % MAX_WATERING_LOGS;
+    if (totalLogs < MAX_WATERING_LOGS) totalLogs++;
+}
 
-// REMEMBER TO REMOVE SOME OF THE COMMENTS :>
-// MAKE SURE TO INSTALL THE MODULES (refer below)
-// Resources for the modules needED for the servo and dht11
-// https://randomnerdtutorials.com/esp32-servo-motor-web-server-arduino-ide/      SERVO TUTS
-// https://esp32io.com/tutorials/esp32-dht11                                      DHT11 TUTS
+void handleRoot() {
+    File file = SPIFFS.open("/index.html", "r");
+    server.streamFile(file, "text/html");
+    file.close();
+}
 
-// For wifi
-// https://randomnerdtutorials.com/esp32-web-server-spiffs-spi-flash-file-system/#:~:text=With%20SPIFFS%2C%20you%20can%20write,documented%20on%20its%20GitHub%20page.
+void handleData() {
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    
+    int moistureRaw = analogRead(SOIL_MOISTURE_PIN);
+    float moisture = map(moistureRaw, 4095, 0, 0, 100);
+    
+    int lightRaw = analogRead(LDR_PIN);
+    float light = map(lightRaw, 4095, 0, 0, 100);
+    
+    StaticJsonDocument<256> doc;
+    doc["temperature"] = isnan(temperature) ? 0 : temperature;
+    doc["humidity"] = isnan(humidity) ? 0 : humidity;
+    doc["moisture"] = moisture;
+    doc["light"] = light;
+    doc["pump"] = digitalRead(PUMP_PIN);
+    doc["growlights"] = digitalRead(GROW_LIGHTS_PIN);
+    doc["fan"] = digitalRead(FAN_PIN);
+    doc["window"] = windowServo.read() > 45;
 
+    String response;
+    serializeJson(doc, response);
+    server.send(200, "application/json", response);
+}
+
+void handleWindow() {
+    if (server.hasArg("open")) {
+        bool open = server.arg("open") == "true";
+        windowServo.write(open ? 90 : 0);
+        server.send(200, "text/plain", "Window position updated");
+    } else {
+        server.send(400, "text/plain", "Missing parameter");
+    }
+}
+
+void handleFan() {
+    if (server.hasArg("on")) {
+        bool on = server.arg("on") == "true";
+        digitalWrite(FAN_PIN, on ? HIGH : LOW);
+        server.send(200, "text/plain", "Fan status updated");
+    } else {
+        server.send(400, "text/plain", "Missing parameter");
+    }
+}
+
+void handleWateringLog() {
+    StaticJsonDocument<2048> doc;
+    JsonArray logs = doc.createNestedArray("logs");
+    
+    int startIndex = currentLogIndex - totalLogs;
+    if (startIndex < 0) startIndex += MAX_WATERING_LOGS;
+    
+    for (int i = 0; i < totalLogs; i++) {
+        int index = (startIndex + i) % MAX_WATERING_LOGS;
+        JsonObject log = logs.createNestedObject();
+        log["timestamp"] = wateringLogs[index].timestamp;
+        log["moisture"] = wateringLogs[index].moisture;
+    }
+    
+    String response;
+    serializeJson(doc, response);
+    server.send(200, "application/json", response);
+}
+
+float getMoisture() {
+    int moistureRaw = analogRead(SOIL_MOISTURE_PIN);
+    return map(moistureRaw, 4095, 0, 0, 100);
+}
